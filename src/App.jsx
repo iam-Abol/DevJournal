08d16ded549fceeaa73eeb3124912015dec6b393
unknown <mh.z77w@gmail.com>
2025-07-26 01:33:19 +0330

    initialize project

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import Sidebar from "./components/Sidebar";
+import { useState } from "react";
+import Goals from "./components/Goals/Goals";
+function App() {
+  return <></>;
+}
+
+export default App;
