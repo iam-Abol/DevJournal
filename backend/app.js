076db16537917a0e456315a3ce2c3897f1ea0680
unknown <mh.z77w@gmail.com>
2025-07-27 23:15:36 +0330

    add journal model

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/devJournal").then(() => {
+  console.log("connected to database");
 
-app.listen(3000, () => {
-  console.log("listening on 3000");
+  app.listen(3000, () => {
+    console.log("listening on 3000");
+  });
 });
---
+++ backend/modals/journal.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const JournalSchema = new mongoose.Schema({
+  title: { type: String, require: true },
+  content: String,
+  createdAi: {
+    type: String,
+    default: () => new Date().toLocaleString(),
+  },
+});
+module.exports = mongoose.model("Journal", JournalSchema);
