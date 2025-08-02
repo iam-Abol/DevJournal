7a998a750c23fd4abd62f545538f6ba31fc04c97
unknown <mh.z77w@gmail.com>
2025-08-02 18:18:07 +0330

    add icons and different links to footer

--- front/src/components/Footer.jsx
+++ front/src/components/Footer.jsx
@@ -1,7 +1,27 @@
+import { Link } from "react-router-dom";
+import { House, BookMarked, Settings, MessagesSquare } from "lucide-react";
 export default function Footer(params) {
   return (
-    <footer class="fixed right-0 bottom-0 left-0 border-t  bg-white shadow-md">
-      <div class="h-16 text-gray-600"></div>
+    <footer className="fixed right-0 bottom-0 left-0 border-t  bg-white shadow-md">
+      <div className="h-16 flex justify-around items-center text-gray-600">
+        <Link className="flex flex-col items-center hover:text-blue-500">
+          <House />
+          <span class="text-xs">Home</span>
+        </Link>
+
+        <Link className="flex flex-col items-center hover:text-blue-500">
+          <MessagesSquare />
+          <span class="text-xs">Messages</span>
+        </Link>
+        <Link className="flex flex-col items-center hover:text-blue-500">
+          <BookMarked />
+          <span class="text-xs">Saved</span>
+        </Link>
+        <Link className="flex flex-col items-center hover:text-blue-500">
+          <Settings />
+          <span class="text-xs">Settings</span>
+        </Link>
+      </div>
     </footer>
   );
 }
