2fd6e97728c1d99fa7dbfc551bf5e76624f290b0
unknown <mh.z77w@gmail.com>
2025-08-02 18:27:16 +0330

    commit

--- front/src/components/Footer.jsx
+++ front/src/components/Footer.jsx
@@ -1,26 +1,34 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { House, BookMarked, Settings, MessagesSquare } from "lucide-react";
 export default function Footer(params) {
   return (
     <footer className="fixed right-0 bottom-0 left-0 border-t  bg-white shadow-md">
-      <div className="h-16 flex justify-around items-center text-gray-600">
-        <Link className="flex flex-col items-center hover:text-blue-500">
+      <div className="h-16 flex justify-around items-center text-gray-500">
+        <NavLink
+          className={() => "flex flex-col items-center hover:text-blue-500"}
+        >
           <House />
           <span class="text-xs">Home</span>
-        </Link>
+        </NavLink>
 
-        <Link className="flex flex-col items-center hover:text-blue-500">
+        <NavLink
+          className={() => "flex flex-col items-center hover:text-blue-500"}
+        >
           <MessagesSquare />
           <span class="text-xs">Messages</span>
-        </Link>
-        <Link className="flex flex-col items-center hover:text-blue-500">
+        </NavLink>
+        <NavLink
+          className={() => "flex flex-col items-center hover:text-blue-500"}
+        >
           <BookMarked />
           <span class="text-xs">Saved</span>
-        </Link>
-        <Link className="flex flex-col items-center hover:text-blue-500">
+        </NavLink>
+        <NavLink
+          className={() => "flex flex-col items-center hover:text-blue-500"}
+        >
           <Settings />
           <span class="text-xs">Settings</span>
-        </Link>
+        </NavLink>
       </div>
     </footer>
   );
