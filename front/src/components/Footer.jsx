6ae3fcbb5a133e38e7f1ec179bd9fdde517dd924
unknown <mh.z77w@gmail.com>
2025-08-03 17:28:10 +0330

    highlight active NavLink in footer navigation

--- front/src/components/Footer.jsx
+++ front/src/components/Footer.jsx
@@ -1,38 +1,49 @@
 import { NavLink } from "react-router-dom";
 import { House, BookMarked, Settings, MessagesSquare } from "lucide-react";
 export default function Footer(params) {
+  let NavLinkStyles = "flex flex-col items-center hover:text-blue-500";
+  let activeClasses = " text-blue-800";
+
   return (
     <footer className="fixed right-0 bottom-0 left-0 border-t  bg-white shadow-md">
-      <div className="h-16 flex justify-around items-center text-gray-500">
+      <div className="h-16 flex justify-around items-center text-gray-400">
         <NavLink
-          className={() => "flex flex-col items-center hover:text-blue-500"}
+          className={({ isActive }) =>
+            NavLinkStyles + (isActive ? activeClasses : "")
+          }
           to="/"
           end
         >
           <House />
-          <span class="text-xs">Home</span>
+          <span className="text-xs">Home</span>
         </NavLink>
 
         <NavLink
-          className={() => "flex flex-col items-center hover:text-blue-500"}
-          to="messages"
+          className={({ isActive }) =>
+            NavLinkStyles + (isActive ? activeClasses : "")
+          }
+          to="/messages"
         >
           <MessagesSquare />
-          <span class="text-xs">Messages</span>
+          <span className="text-xs">Messages</span>
         </NavLink>
         <NavLink
-          className={() => "flex flex-col items-center hover:text-blue-500"}
-          to="saved"
+          className={({ isActive }) =>
+            NavLinkStyles + (isActive ? activeClasses : "")
+          }
+          to="/saved"
         >
           <BookMarked />
-          <span class="text-xs">Saved</span>
+          <span className="text-xs">Saved</span>
         </NavLink>
         <NavLink
-          className={() => "flex flex-col items-center hover:text-blue-500"}
-          to="settings"
+          className={({ isActive }) =>
+            NavLinkStyles + (isActive ? activeClasses : "")
+          }
+          to="/settings"
         >
           <Settings />
-          <span class="text-xs">Settings</span>
+          <span className="text-xs">Settings</span>
         </NavLink>
       </div>
     </footer>
