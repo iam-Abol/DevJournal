2bb881e9924276d0fc375f5a964a3890ca866f23
unknown <mh.z77w@gmail.com>
2025-08-18 17:25:21 +0330

    feat(ui): add styled error element with close functionality

--- front/src/components/auth/Signup.jsx
+++ front/src/components/auth/Signup.jsx
@@ -41,13 +41,43 @@ export default function Signup({}) {
   console.log(error);
   let content = null;
   if (error?.type == "validation")
-    content = error.errors.map((err) => <p>{err.message}</p>);
+    content = error.errors.map((err) => (
+      <div
+        class="flex mx-6 justify-between bg-red-100 border border-red-400 text-red-700 px-4 py-3 my-2 rounded  "
+        role="alert"
+      >
+        <span class="block sm:inline pl-2">
+          <strong class="font-bold">{err.message}</strong>
+        </span>
+        <span
+          class="inline"
+          onClick={() =>
+            setError((prevError) => {
+              const errors = prevError.errors.filter(
+                (e) => e.message != err.message
+              );
+              return { ...prevError, errors };
+            })
+          }
+        >
+          <svg
+            class="fill-current h-6 w-6"
+            role="button"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+          >
+            <title>Close</title>
+            <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+          </svg>
+        </span>
+      </div>
+    ));
 
   return (
     <>
-      {content}
       <div className="bg-blue">
-        <div className="flex min-h-screen bg-white">
+        {content}
+        <div className="flex  min-h-screen bg-white">
           <div className="md:w-1/2 max-w-lg mx-auto my-24 px-4 py-5 shadow-none">
             <div className="text-center p-0 font-sans ">
               <h1 className=" text-gray-800 text-3xl font-medium">
